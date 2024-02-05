2dbcb78c0c1a6f3f37ba913ac229d066b1b68a63
alan_omam <omamalan@hotmail.it>
2024-02-05 15:48:11 +0100

    FATTO SNACK1 (sostituito il ciclo for con un while sistemando cosi la parte logica, poi ho messo i risultati in DOM)

--- snack1/index.js
+++ snack1/index.js
@@ -6,12 +6,28 @@ a inserirli nell’array fino a quando la somma degli elementi è minore di 50
 */
 
 
+
+
+
+
+let arrayOutput = document.querySelector("#array")
+let sommaOutput = document.querySelector("#sum")
+
 let numeri = []
 let somma = 0
-for(let i = 0; i<10; i++){
+let n = 0
+
+
+while(somma < 50){
 let numero = Number(prompt("inserisci un numero"))
+
 numeri.push(numero)
-somma = somma + numeri[i]
+somma = somma + numeri[n]
+
+n++
 }
 
+arrayOutput.innerHTML = numeri
+sommaOutput.innerHTML = somma
 console.log(somma)
+console.log(numeri)
