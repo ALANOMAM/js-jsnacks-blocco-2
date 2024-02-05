7922d2875027db9aa43ab8a4ba02bc1783497df1
alan_omam <omamalan@hotmail.it>
2024-02-05 17:00:57 +0100

    fatto la logica dello snack 4

---
+++ snack4/index.js
@@ -0,0 +1,15 @@
+/*
+Snack 4
+Inserisci un numero, se è pari stampa il numero, 
+se è dispari stampa il numero successivo.
+*/ 
+
+
+
+ let numero = Number(prompt("inserisci un numero"))
+
+ if(numero % 2 == 0){
+     console.log(numero)
+ }else{
+    console.log(numero+1)
+ }
